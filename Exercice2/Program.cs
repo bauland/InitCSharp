af8e5cd945b6f8411391609f58d1a007022a4777
Nicolas Bauland <nicolas.bauland@laposte.net>
2019-05-13 16:32:17 +0200

    Exercice 2: fonction récursive

---
+++ Exercice2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Exercice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Fonctions récursives
+            //
+            // Créer un programme pour afficher le résultat de la suite de Fibonacci pour 25.
+            //
+            // Rappels:
+            // - pour afficher dans la fenêtre de la console:
+            //      Console.WriteLine(s);
+            // - pour afficher dans la fenêtre de débogage:
+            //      Debug.WriteLine(s);
+            //
+            // avec s la chaine de caractere à afficher
+            // 
+
+            var resultat = Fibonacci(25);
+            Console.WriteLine(resultat);
+            Debug.WriteLine(resultat);
+            PauseDeFin();
+        }
+
+        static int Fibonacci(int n)
+        {
+            // TODO: A compléter
+
+        }
+
+        private static void PauseDeFin()
+        {
+            Console.WriteLine("Appuyez sur une touche pour terminer le programme ...");
+            Console.ReadKey();
+        }
+    }
+}
