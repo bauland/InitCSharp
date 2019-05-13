c026fb1c9e900125427c06c486c7c9a8e5b73d5d
Nicolas Bauland <nicolas.bauland@laposte.net>
2019-05-13 16:16:15 +0200

    Exercice 1: les boucles for

--- Exercice1/Program.cs
+++ Exercice1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Exercice1
 {
@@ -6,7 +7,28 @@ namespace Exercice1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Boucle for
+            //
+            // Créer un programme pour afficher les nombres pairs de 0 à 100.
+            //
+            // Rappels:
+            // - pour afficher dans la fenêtre de la console:
+            //      Console.WriteLine(s);
+            // - pour afficher dans la fenêtre de débogage:
+            //      Debug.WriteLine(s);
+            //
+            // avec s la chaine de caractere à afficher
+
+            // TODO: A compléter
+
+
+            PauseDeFin();
+        }
+
+        private static void PauseDeFin()
+        {
+            Console.WriteLine("Appuyez sur une touche pour terminer le programme ...");
+            Console.ReadKey();
         }
     }
 }
